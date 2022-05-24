cd2941cb0af900dff7b8f12a362b748a779e61b1
Trevor Manz <trevor.j.manz@gmail.com>
2022-05-24 08:27:05 -0400

    refactor: complete JSDoc annotation

--- index.js
+++ index.js
@@ -3,7 +3,32 @@ import OAuth from "oauth-1.0a";
 import HmacSHA1 from "crypto-js/hmac-sha1.js";
 import Base64 from "crypto-js/enc-base64.js";
 
+/**
+ * @typedef Env
+ * @prop {string} TWITTER_API_KEY
+ * @prop {string} TWITTER_API_SECRET
+ * @prop {string} TWITTER_ACCESS_TOKEN
+ * @prop {string} TWITTER_ACCESS_TOKEN_SECRET
+ */
+
+/**
+ * @typedef MediaUploadResponse
+ * @prop {number} media_id
+ * @prop {string} media_id_string
+ * @prop {number} size
+ * @prop {number} expires_after_secs
+ * @prop {{ image_type: string, w: number, h: number }} image
+ */
+
+/**
+ * Minimial typings of status update response... The actual payload is much much larger.
+ *
+ * @typedef StatusUpdateResponse
+ * @prop {{ media: { display_url: string }[] }} entities
+ */
+
 class Twitter {
+	/** @param {{ consumer: OAuth.Consumer, token: OAuth.Token }} options */
 	constructor({ consumer, token }) {
 		this.auth = new OAuth({
 			consumer,
@@ -18,95 +43,72 @@ class Twitter {
 	/**
 	 * @param {'api' | 'upload'} subdomain
 	 * @param {string} resource
-	 * @param {Record<string, any>} data
+	 * @param {Record<string, string>} data
 	 */
 	async post(subdomain, resource, data) {
+		let method = "POST";
 		let url = `https://${subdomain}.twitter.com/1.1/${resource}`;
-		let request = { method: "POST", url, data };
-		let response = await fetch(request.url, {
-			method: request.method,
+		let response = await fetch(url, {
+			method,
+			body: new URLSearchParams(data),
 			headers: {
 				"Content-Type": "application/x-www-form-urlencoded",
 				"Accept": "application/json",
-				...this.auth.toHeader(this.auth.authorize(request, this.token)),
+				...this.auth.toHeader(
+					this.auth.authorize({ url, method, data }, this.token),
+				),
 			},
-			body: new URLSearchParams(request.data),
 		});
 		return response.json();
 	}
-	/**
-	 * @typedef {{
-	 *   media_id: number;
-	 *   media_id_string: string;
-	 *   size: number;
-	 *   expires_after_secs: number;
-	 *   image: { image_type: string, w: number, h: number };
-	 * }} MediaUploadResponse
-	 */
 
 	/**
 	 * @param {string} media base64 encoded media
 	 * @return {Promise<MediaUploadResponse>}
 	 */
-	upload_media(media) {
+	upload(media) {
 		return this.post("upload", "media/upload.json", { media });
 	}
 
 	/**
-	 * @typedef {{
-	 *    entities: { media: { display_url: string }[] },
-	 *  }} StatusUpdateResponse
-	 *
-	 * Minimial typings of API response... The actual payload is much much larger.
-	 */
-	/**
-	 * @param {string} media_id
+	 * @param {string} status
+	 * @param {string=} media_id
 	 * @return {Promise<StatusUpdateResponse>}
 	 */
-	tweet(media_id) {
+	tweet(status, media_id) {
 		return this.post("api", "statuses/update.json", {
-			status: "BioVis Copy",
+			status,
 			media_ids: media_id,
 		});
 	}
 }
 
-/**
- * @param {Request} request
- *
- * Check if we are dealing with a CORS preflight request (https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request)
- */
-function is_preflight(request) {
-	return (
-		request.method === "OPTIONS" &&
-		request.headers.get("Origin") !== null &&
-		request.headers.get("Access-Control-Request-Method") !== null &&
-		request.headers.get("Access-Control-Request-Headers") !== null
-	);
-}
-
 export default {
 	/**
+	 * Cloudflare Worker [Module](https://developers.cloudflare.com/workers/wrangler/module-system/)
+	 *
 	 * @param {Request} request
-	 * @param {{
-	 *   TWITTER_API_KEY: string,
-	 *   TWITTER_API_SECRET: string,
-	 *   TWITTER_ACCESS_TOKEN: string,
-	 *   TWITTER_ACCESS_TOKEN_SECRET: string
-	 * }} env
+	 * @param {Env} env
 	 */
 	async fetch(request, env) {
 		if (request.method === "OPTIONS") {
-			let headers = is_preflight(request)
-				? {
-					"Access-Control-Allow-Origin": "*",
-					"Access-Control-Allow-Methods": "POST",
-					"Access-Control-Max-Age": "86400",
-					"Access-Control-Allow-Headers": request.headers.get(
-						"Access-Control-Request-Headers",
-					),
-				}
-				: { Allow: "POST" };
+			let headers = new Headers();
+			// Check if we are dealing with a CORS preflight request (https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request)
+			if (
+				request.headers.get("Origin") !== null &&
+				request.headers.get("Access-Control-Request-Method") !== null &&
+				request.headers.get("Access-Control-Request-Headers") !== null
+			) {
+				headers.append("Access-Control-Allow-Origin", "*");
+				headers.append("Access-Control-Allow-Methods", "POST");
+				headers.append("Access-Control-Max-Age", "86400");
+				headers.append(
+					"Access-Control-Allow-Headers",
+					request.headers.get("Access-Control-Request-Headers"),
+				);
+			} else {
+				headers.append("Allow", "POST");
+			}
 			return new Response(null, { status: 204, headers });
 		}
 
@@ -118,7 +120,10 @@ export default {
 		}
 
 		let client = new Twitter({
-			consumer: { key: env.TWITTER_API_KEY, secret: env.TWITTER_API_SECRET },
+			consumer: {
+				key: env.TWITTER_API_KEY,
+				secret: env.TWITTER_API_SECRET,
+			},
 			token: {
 				key: env.TWITTER_ACCESS_TOKEN,
 				secret: env.TWITTER_ACCESS_TOKEN_SECRET,
@@ -126,13 +131,13 @@ export default {
 		});
 
 		let url = await request.json()
-			.then((body) => client.upload_media(body.data))
-			.then((media) => client.tweet(media.media_id_string))
+			.then((json) => client.upload(json.data))
+			.then((media) => client.tweet("BioVis Copy", media.media_id_string))
 			.then((tweet) => tweet.entities.media[0].display_url);
 
 		return new Response(JSON.stringify({ url }), {
 			headers: {
-				"content-type": "application/json",
+				"Content-Type": "application/json",
 				"Access-Control-Allow-Origin": "*",
 				"Access-Control-Allow-Methods": "POST",
 			},
